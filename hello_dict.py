bdae393032681bd2d3071b4719849410cce0d705
falencarlima <falencarlima@gmail.com>
2022-06-15 22:09:52 -0400

    Refatorando o script hello.py com uso de dict e criando um novo arquivo, hello_dict.py.

---
+++ hello_dict.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""Hello World Multi Linguas.
+
+Dependendo da lingua configurada no ambiente o programa exibe a mensagem
+correspondente.
+
+Como usar:
+
+Tenha a variável LANG devidamente configurada ex:
+    export LANG=pt_BR
+
+Execução:
+    python3 hello.py
+    ou
+    ./hello.py
+"""
+
+__version__ = "0.1.2"
+__author__ = "Fábio Alencar"
+__license__ = "Unlicense"
+
+import os
+
+current_language = os.getenv("LANG", "en_US")[:5]
+msg = {
+	"en_US": "Hello, World!",
+	"pt_BR": "Olá, Mundo!",
+	"it_IT": "Ciao, Mondo!",
+	"es_SP": "Hola, Mundo!",
+	"fr_FR": "Bonjour Monde!",
+}
+ 
+print(msg[current_language])
